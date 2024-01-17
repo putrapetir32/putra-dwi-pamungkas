6e37fe83b7ba654480240ce14f3352c856c9cb13
putrapetir32 <144751101+putrapetir32@users.noreply.github.com>
2024-01-17 12:20:23 +0700

    Add files via upload

---
+++ tugas-labs/28_object_a.js
@@ -0,0 +1,25 @@
+let murid = {
+    nis:"21223457",
+    nama: "aramugam",
+    alamat:"malaysia",
+    nilai:"78",
+}
+
+console.log("nis   :" + murid.nis)
+console.log("nama   :" + murid.nama)
+console.log("alamat  : " + murid.alamat)
+console.log("nilai     :"  + murid.nilai)
+
+if(nilai = "80"){
+    keterangan="nilai anda cukup"
+} else if (nilai = "72"){
+    keterangan="nilai anda cukup"
+} else if (nilai = "90") {
+    keterangan="nilai anda lulus"
+} else if (nilai ="60"){
+    keterangan="nilai anda tidak cukup"
+} else if (nilai = "78"){
+    keterangan ="nilai anda lulus"
+}
+console.log(`Nilai anda ${murid.nilai}`, `Dengan Keterangan ${keterangan}`);
+
---
+++ tugas-labs/28_object_b.js
@@ -0,0 +1,14 @@
+let nama = prompt("nama: ");
+let beratBadan = prompt("berat badan anda");
+
+if(beratBadan >= 80){
+    alert("berat anda")
+} else if(beratBadan >= 50){
+    alert("berat anda normal")
+} else if (beratBadan >= 34){
+    alert("berat anda tidak normal")
+} else if (beratBadan >= 56){
+    alert("berat anda normal")
+} else {
+    alert("kapan meninggal")
+}
